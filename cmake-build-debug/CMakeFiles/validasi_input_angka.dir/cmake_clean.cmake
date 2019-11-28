90e8cea6f6dd4fd539877597d92bda56a918448f
adarmawan117 <adarmawan117@gmail.com>
2019-11-28 09:31:13 +0700

    COMMIT PERTAMA.

---
+++ cmake-build-debug/CMakeFiles/validasi_input_angka.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/validasi_input_angka.dir/main.c.obj"
+  "validasi_input_angka.pdb"
+  "validasi_input_angka.exe"
+  "validasi_input_angka.exe.manifest"
+  "libvalidasi_input_angka.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/validasi_input_angka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/validasi_input_angka.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for validasi_input_angka.
+# This may be replaced when dependencies are built.
